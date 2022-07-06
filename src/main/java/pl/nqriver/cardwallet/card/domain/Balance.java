7b10fd84044bc8edee8e0d09abe8d52a31ee92ca
nqriver <nqriver@gmail.com>
2022-07-06 23:50:09 +0200

    Adding static factory method for Balance simplified model

--- src/main/java/pl/nqriver/cardwallet/card/domain/Balance.java
+++ src/main/java/pl/nqriver/cardwallet/card/domain/Balance.java
@@ -2,19 +2,32 @@ package pl.nqriver.cardwallet.card.domain;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import pl.nqriver.cardwallet.card.domain.LoyaltyCard.LoyaltyCardId;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
 @Getter
 public class Balance {
-    private final Points overallBalance;
+    private final LoyaltyCardId loyaltyCardId;
+    private final Points totalBalance;
     private final Points withdrawalBalance;
     private final Points depositBalance;
 
     public static Balance of(LoyaltyCard card) {
-        return new Balance(
-                card.getActivityWindow().calculateBalance(),
-                card.getActivityWindow().calculateWithdrawalBalance(),
-                card.getActivityWindow().calculateDepositBalance());
+        return Balance.builder()
+                .loyaltyCardId( card.getId().get())
+                .totalBalance(card.calculateBalance())
+                .depositBalance(card.getActivityWindow().calculateDepositBalance())
+                .depositBalance(card.getActivityWindow().calculateWithdrawalBalance())
+                .build();
+    }
+
+    public static Balance ofTotal(LoyaltyCard card) {
+        return Balance.builder()
+                .loyaltyCardId(card.getId().get())
+                .totalBalance(card.calculateBalance())
+                .build();
     }
 }
